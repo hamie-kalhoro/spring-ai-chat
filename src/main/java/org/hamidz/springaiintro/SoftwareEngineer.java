5d98b72a8b2bd68472d9181ed01dfd47df3a628c
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-05-23 15:55:00 +0500

    adds Entity class and Repository class for Software Engineer and also properties for application file

---
+++ src/main/java/org/hamidz/springaiintro/SoftwareEngineer.java
@@ -0,0 +1,30 @@
+package org.hamidz.springaiintro;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Objects;
+
+@Entity
+@Setter @Getter @NoArgsConstructor @AllArgsConstructor
+public class SoftwareEngineer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
